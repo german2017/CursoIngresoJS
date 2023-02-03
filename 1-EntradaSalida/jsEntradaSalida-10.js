737ab8310aac2eeab9d8abd37ab3a792d40a5a71
german2017 <scarafiloutn@gmail.com>
2023-02-03 14:28:52 -0300

    Modifique el tp2 agregue archivo nuevo

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var calculo;
+	var radio;
+
+	radio = 5;
+
+	//lert(Math.PI);
+
+	calculo = Math.PI * 2 * radio;
+
+	alert (calculo);
 }
